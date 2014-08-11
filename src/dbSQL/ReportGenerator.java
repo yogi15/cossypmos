be40882a4f20ccc6b6c3e545ac75fa36da080b1d
pankajmahadik24@gmail.com <pankajmahadik24@gmail.com@270f5cef-2aaa-b512-6b27-43a3548ac535>
2014-08-11 07:28:11 +0000

    ReportGenerator.java : Add code for ForwardLadder

--- src/dbSQL/ReportGenerator.java
+++ src/dbSQL/ReportGenerator.java
@@ -9,6 +9,7 @@ import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -54,17 +55,68 @@ public class ReportGenerator {
 
 	
 	
-	
+	public Collection generateReportOnSQLOnStatement(Vector resulstSetvector) throws RemoteException {
+		// TODO Auto-generated method stub
+		ResultSet rs = null;
+		int columnCount = 0;
+		 try {
+		Vector v1 = resulstSetvector;
+		//if(v1.size() > 0) {
+		
+			for(int r=0;r<v1.size();r++) {
+				PreparedStatement stat = (PreparedStatement) v1.get(r);
+				rs = stat.executeQuery();
+			   
+				ResultSetMetaData mdata = rs.getMetaData();
+				columnCount = mdata.getColumnCount();
+				for(int i=0;i<columnCount ;i++) {
+				//	System.out.println(mdata.getColumnLabel(i + 1));
+				}
+				// if(r ==0)  {
+				for(int i=0;i<columnCount ;i++) {
+					
+				//	System.out.println(mdata.getColumnName(i + 1));
+					// don't consider this rule for Attribute table. 
+					if(mdata.getColumnName(i + 1).contains("id") || mdata.getColumnName(i + 1).contains("ID")) 
+						if(!mdata.getColumnLabel(i + 1).equalsIgnoreCase("ORDERID")) 
+						  setType("java.lang.Integer");
+						else 
+							setType(mdata.getColumnClassName(i+1));
+					else
+					setType(mdata.getColumnClassName(i+1));
+					setHeader(mdata.getColumnName(i + 1));
+				}
+			//	 }
+				
+				processtableData(rs,columnCount,r); 
+				
+				stat.close();
+			 }
+			
+				v1.removeAllElements();
+				v1.add(header);
+				v1.add(types);
+				v1.add(tableData);
+				return v1;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				commonUTIL.displayError("ReportImp", "generateReportOnSQL" , e);
+			}
+			 
+		
+		 return null;
+	}
 	
 	public Collection generateReportOnSQL(Vector resulstSetvector) throws RemoteException {
 		// TODO Auto-generated method stub
 		ResultSet rs = null;
 		int columnCount = 0;
-		
+		 try {
 		Vector v1 = resulstSetvector;
-		if(v1.size() > 0) {
-			 rs = (ResultSet) v1.get(0);
-			 try {
+		//if(v1.size() > 0) {
+			for(int r=0;r<v1.size();r++) {
+			 rs = (ResultSet) v1.get(r);
+			
 				ResultSetMetaData mdata = rs.getMetaData();
 				columnCount = mdata.getColumnCount();
 				for(int i=0;i<columnCount ;i++) {
@@ -84,6 +136,8 @@ public class ReportGenerator {
 					setHeader(mdata.getColumnName(i + 1));
 				}
 				processtableData(rs,columnCount); 
+			 }
+		
 				v1.removeAllElements();
 				v1.add(header);
 				v1.add(types);
@@ -94,10 +148,64 @@ public class ReportGenerator {
 				commonUTIL.displayError("ReportImp", "generateReportOnSQL" , e);
 			}
 			 
-		}
+		
 		 return null;
 	}
-
+	private void processtableData(ResultSet rs, int columnCount,int r) {
+		// TODO Auto-generated method stub
+		try {
+			while(rs.next()) {
+				 final Vector<Object> record = new Vector<Object>();
+			for(int i=0;i<columnCount;i++) {
+				final Class<?> classType = getDataType()[i];
+			//	System.out.println(classType + " at " + i);
+				if (classType.equals(Date.class)) {
+	                try {
+	                    // Sun Mar 13 19:00:00 CDT 2011
+	                    // EEE MMM dd HH:mm:ss z yyyy
+	                    final Date date = FORMAT.parse(rs.getString(i+1));
+	                    record.add(date); // order date
+	                } catch (final ParseException e) {
+	                    e.printStackTrace();
+	                }
+		            } else if (classType.equals(String.class)) {
+		            	record.add(rs.getString(i+1));
+		
+		            } else if (classType.equals(double.class)
+		                || classType.equals(Double.class)) {
+		            	record.add(Double.valueOf(rs.getString(i+1)));
+		
+		            } else if (classType.equals(int.class)
+		                || classType.equals(Integer.class)) {
+		            	record.add(Integer.valueOf(rs.getString(i+1)));
+		
+		            } else if (classType.equals(Math.class)
+		                || classType.equals(Math.class)) {
+		            	record.add(Integer.valueOf(rs.getString(i+1)));
+		
+		            } else if (classType.equals(BigDecimal.class)
+		                || classType.equals(BigDecimal.class)) {
+		            	String value = rs.getString(i+1);
+		            	if(value.contains("."))
+		            	       record.add(Double.valueOf(rs.getString(i+1)));
+		            	else 
+		            		 record.add(Integer.valueOf(rs.getString(i+1)));
+		
+		            } else {
+		            	record.add(rs.getString(i+1));
+		            }
+ 			
+	}
+			tableData.add(record);
+				
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			commonUTIL.displayError("ReportImp", "processtableData" , e);
+		}
+		
+	}
 	private void processtableData(ResultSet rs, int columnCount) {
 		// TODO Auto-generated method stub
 		try {
