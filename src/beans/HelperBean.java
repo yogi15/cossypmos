a7518c582485498089593445a213f4fe33509ace
yogi.kadam <yogi.kadam@gmail.com>
2014-08-25 15:02:50 +0000

    HelperBean: method setColumnNames() added

--- src/beans/HelperBean.java
+++ src/beans/HelperBean.java
@@ -1,6 +1,9 @@
 package beans;
 
+import java.util.ArrayList;
+
 public interface HelperBean {
 	public  Deal  getBean(String [] record);
+	public  void  setColumnNames(ArrayList<String> columnNames);
 
 }
