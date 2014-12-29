e3812087dd2fd6a910b0d634d226528a3bfb8b2f
pankajmahadik24 <pankajmahadik24@gmail.com>
2014-12-29 16:39:41 +0000

    BOFXFORWARDMessageHandler.java added code for SDI selectioner

--- src/bo/message/bomessagehandler/BOFXFORWARDMessageHandler.java
+++ src/bo/message/bomessagehandler/BOFXFORWARDMessageHandler.java
@@ -1,21 +1,27 @@
 package bo.message.bomessagehandler;
 
+import java.util.Vector;
+
 import util.commonUTIL;
 import dsEventProcessor.TradeEventProcessor;
 import dsEventProcessor.TransferEventProcessor;
 import beans.LegalEntity;
 import beans.Message;
 import beans.MessageConfig;
+import beans.Sdi;
 import beans.Trade;
 import beans.Transfer;
+import bo.util.SDISelectorUtil;
 
 public class BOFXFORWARDMessageHandler extends BOMessageHandler  {
 	
 	
 
 	public Message fillMessage(Trade trade,Transfer transfer,MessageConfig mConfig,String trrigerON,TransferEventProcessor transferEvent,TradeEventProcessor tradeEvent,LegalEntity receiver,LegalEntity po) {
-		
-		Message message = new Message();
+		Message message = null;
+		try {
+			message =  new Message();
+			message.setMessageConfigID(mConfig.getId());
 		message.setTradeId(trade.getId());
 		message.setProductType(mConfig.getProductType());
 		if(transfer != null ) {
@@ -34,21 +40,47 @@ public class BOFXFORWARDMessageHandler extends BOMessageHandler  {
 		message.setMessageType(mConfig.getMessageType());
 		message.setproductID(trade.getProductId());
 		message.setTradeVersion(trade.getVersion());
-		message.setReceiverName(receiver.getName());
-		message.setReceiverRole(receiver.getRole());
+		if(receiver == null)
+			message.setReceiverName("");
+		else 
+			message.setReceiverName(receiver.getName());
+		
+		message.setReceiverRole(mConfig.getReceiverRole());
 		message.setSenderName(po.getName());
-		message.setSenderRole("PO");
+		message.setSenderRole("PO"); // i have doubt on the same. 
 		message.setTemplateName(mConfig.getTemplateName());
 		message.setFormat(mConfig.getFormatType());
 		message.setAddressType(mConfig.getAddressType());
 		message.setSenderContactType(mConfig.getPoContactType());
 		message.setReceiverContactType(mConfig.getReceiverContactType());
+		if(mConfig.getReceiverID() == 0) {
+			if(mConfig.getReceiverRole().equalsIgnoreCase("CounterParty")) {
+				message.setReceiverId(trade.getCpID());
+			} else {
+					
+				if(message.getTriggerON().equalsIgnoreCase("TRADE")) {
+					String key = "PO|"+trade.getCurrency()+"|"+trade.getProductType()+"|"+String.valueOf(mConfig.getPoid());
+					 Vector<Sdi> sdiperferred = SDISelectorUtil.getPreferredSdisOnKey(key);
+					 if(sdiperferred != null) {
+						 Sdi sdi = sdiperferred.elementAt(0);
+						 message.setReceiverId(sdi.getAgentId());
+					 } else {
+						 message.setReceiverId(0); // no sdi found. 
+					 }
+					 
+				}
+			}
+		} else {
 		message.setReceiverId(mConfig.getReceiverID());
+		}
 		message.setSenderId(mConfig.getPoid());
 	//	message.setSenderAddressCodet)
 		
 		//message.set
-			
+		}catch (NullPointerException e) {
+			commonUTIL.displayError("BOFXForwardHandler", "fillMessage", e);
+			return message;
+		}
 		return message;
 		
 	}
