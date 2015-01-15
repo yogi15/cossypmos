6b737fa3b1551feb5be1ceb3534e096b3c8f3193
pankajmahadik24@gmail.com <pankajmahadik24@gmail.com@270f5cef-2aaa-b512-6b27-43a3548ac535>
2015-01-15 19:26:03 +0000

    SQLAppender

---
+++ src/logAppender/SQLAppender.java
@@ -0,0 +1,32 @@
+package logAppender;
+
+import org.apache.log4j.Logger;
+
+import constants.logConstants;
+
+public class SQLAppender {
+	static Logger logger = Logger.getLogger(
+		SQLAppender.class.getName());
+
+
+
+	
+	public static void printLog(String logType, String msg) {
+
+		switch (logConstants.logTypes.get(logType)) {
+
+		case 1:
+			logger.info(msg);
+			return;
+			
+		case 2:
+			logger.error(msg);
+			
+			return;
+
+		}
+
+	}
+	
+
+}
