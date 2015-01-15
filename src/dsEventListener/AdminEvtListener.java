dd787883166b30e5971f262d35ccd41516a222fb
pankajmahadik24@gmail.com <pankajmahadik24@gmail.com@270f5cef-2aaa-b512-6b27-43a3548ac535>
2015-01-15 18:31:00 +0000

    AdminEvtListener.java, EngineMonitorEvtListener,MessageEvtListener

---
+++ src/dsEventListener/AdminEvtListener.java
@@ -0,0 +1,32 @@
+package dsEventListener;
+
+import dsEventProcessor.EventProcessor;
+import dsEventProcessor.TaskEventProcessor;
+import dsManager.ControllerManager;
+
+public class AdminEvtListener extends ControllerManager {
+	String managerName = " AdminListener";
+    Thread adminManagerThread = null;
+    apps.window.adminmonitor.AdminMonitorWindow adminEvntConsumer = null;
+	public Thread getManagerThread() {
+		return adminManagerThread;
+	}
+	public  AdminEvtListener(String host, String hostName, String managerName) {
+		super(host, hostName, managerName);
+		// TODO Auto-generated constructor stub
+	}
+	public void setAdminEvtListener(apps.window.adminmonitor.AdminMonitorWindow transferEvtConsumer) {
+		this.adminEvntConsumer = transferEvtConsumer;
+	
+				
+			//System.out.println(remoteBORef);
+		
+	   	 } 
+	public void handleEvent(EventProcessor event)	 {
+	
+					adminEvntConsumer.addtaskData(event);
+			
+		
+	}
+	
+}
---
+++ src/dsEventListener/EngineMonitorEvtListener.java
@@ -0,0 +1,33 @@
+package dsEventListener;
+
+import dsEventProcessor.EventProcessor;
+import dsManager.ControllerManager;
+import dsServices.EngineMonitorService;
+
+public class EngineMonitorEvtListener extends ControllerManager {
+	String managerName = "EngineMonitorEvtListener";
+    Thread engineMonitorThread = null;
+    EngineMonitorService engineMonitorService = null;
+	public Thread getManagerThread() {
+		return engineMonitorThread;
+	}
+	public  EngineMonitorEvtListener(String host, String hostName, String managerName) {
+		super(host, hostName, managerName);
+		// TODO Auto-generated constructor stub
+	}
+	public void setEngineMonitorService(EngineMonitorService engineMonitorService ) {
+		this.engineMonitorService = engineMonitorService;
+	
+				
+			//System.out.println(remoteBORef);
+		
+	   	 } 
+	public void handleEvent(EventProcessor event)	 {
+	
+		engineMonitorService.monitorLiveEngineService(event);
+			
+		
+	}
+	
+
+}
