7ad3defcb37f517479e5e44dd735d4746385bc36
pankajmahadik24 <pankajmahadik24@gmail.com>
2015-01-16 12:39:35 +0000

    TransferServiceAppenderLog

---
+++ src/logAppender/TransferServiceAppenderLog.java
@@ -0,0 +1,36 @@
+package logAppender;
+
+import org.apache.log4j.Logger;
+
+import constants.logConstants;
+
+public class TransferServiceAppenderLog {
+	
+	static Logger logger = Logger.getLogger(
+			TransferServiceAppenderLog.class.getName());
+	
+	public static void printLog(String logType, String msg) {
+
+		switch (logConstants.logTypes.get(logType)) {
+
+		case 1:
+			logger.info(msg);
+			return;
+		case 2:
+			logger.debug(msg);
+			
+			return;
+
+		
+		case 3:
+			logger.error(msg);
+			
+			return;
+
+		}
+
+	}
+	
+
+
+}
