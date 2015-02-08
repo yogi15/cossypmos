baf6daed3b93deccaeeb71becacf8a53906bbdbc
yogi.kadam@gmail.com <yogi.kadam@gmail.com@270f5cef-2aaa-b512-6b27-43a3548ac535>
2015-02-08 16:36:59 +0000

    SearchCriteriaType: processCurrenyPairData() method added

--- src/apps/window/reportwindow/SearchCriteriaType.java
+++ src/apps/window/reportwindow/SearchCriteriaType.java
@@ -13,15 +13,14 @@ import javax.swing.JPanel;
 import com.jidesoft.combobox.MultiSelectListExComboBox;
 
 import util.commonUTIL;
-
 import dsServices.RemoteBOProcess;
 import dsServices.RemoteReferenceData;
 import dsServices.RemoteTask;
 import dsServices.RemoteTrade;
 import dsServices.ServerConnectionUtil;
-
 import apps.window.operationwindow.jobpanl.FilterValues;
 import beans.Book;
+import beans.CurrencyPair;
 import beans.FilterBean;
 import beans.LegalEntity;
 import beans.StartUPData;
@@ -58,8 +57,7 @@ public abstract class SearchCriteriaType extends JPanel
  				commonUTIL.getServerIP());
 
  		try {
- 			remoteBORef = (RemoteReferenceData) de
- 					.getRMIService("ReferenceData");
+ 			remoteBORef = (RemoteReferenceData) de.getRMIService("ReferenceData");
  			remoteTask = (RemoteTask) de.getRMIService("Task");
  			remoteTrade = (RemoteTrade) de.getRMIService("Trade");
  			remoteBo = (RemoteBOProcess) de.getRMIService("BOProcess");
@@ -70,16 +68,13 @@ public abstract class SearchCriteriaType extends JPanel
 		}
     	 
      }
+     
      public SearchCriteriaType(RemoteTrade remoteTrade,RemoteReferenceData remoteRef,RemoteBOProcess remoteBO,RemoteTask remoteTask) {
-    	
- 		
- 			this.remoteBORef =  remoteRef;
- 			this.remoteTask = remoteTask;
- 			this.remoteTrade =  remoteTrade;
- 			this.remoteBo = remoteBO;
- 			filterValues = new FilterValues(remoteBORef, remoteTrade,remoteTask, remoteBo);
- 		
-    	 
+    	 this.remoteBORef =  remoteRef;
+ 		 this.remoteTask = remoteTask;
+ 		 this.remoteTrade =  remoteTrade;
+ 		 this.remoteBo = remoteBO;
+ 		 filterValues = new FilterValues(remoteBORef, remoteTrade,remoteTask, remoteBo);    	 
      }
      
      
@@ -89,7 +84,7 @@ public abstract class SearchCriteriaType extends JPanel
  			@Override
  			public void itemStateChanged(ItemEvent e) {
  				if (values.getSelectedIndex() != -1) {
- 					// TODO Auto-generated method stub
+ 					
  					String attributeName = values.getSelectedItem().toString();
  					String idSelected ="";
  					int ids [] = values.getSelectedIndices();
@@ -197,26 +192,22 @@ public abstract class SearchCriteriaType extends JPanel
      public FilterBean getBUYSELL(String tradeType)  {
     	 FilterBean bean = null;
     	 if(!commonUTIL.isEmpty(tradeType)) {
-    	 bean = new FilterBean();
-			bean.setColumnName("Type");
-			bean.setColumnValues(tradeType);
-			bean.setSearchCriteria("in");
-			bean.setAnd_or("And");
-		
- 			
+    		 bean = new FilterBean();
+    		 bean.setColumnName("Type");
+    		 bean.setColumnValues(tradeType);
+    		 bean.setSearchCriteria("in");
+    		 bean.setAnd_or("And"); 			
  		}
-    	 return bean;
+    	return bean;
      }
      public FilterBean getLadder(String ladder,String Ladder)  {
     	 FilterBean bean = null;
     	 if(!commonUTIL.isEmpty(ladder)) {
-    	 bean = new FilterBean();
-			bean.setColumnName(Ladder);
-			bean.setColumnValues(ladder);
-			bean.setSearchCriteria("in");
-			bean.setAnd_or("And");
-		
- 			
+    		 bean = new FilterBean();
+    		 bean.setColumnName(Ladder);
+    		 bean.setColumnValues(ladder);
+    		 bean.setSearchCriteria("in");
+    		 bean.setAnd_or("And"); 			
  		}
     	 return bean;
      }
@@ -224,11 +215,10 @@ public abstract class SearchCriteriaType extends JPanel
     	 FilterBean bean = null;
     	 if(!commonUTIL.isEmpty(Status)) {
     		 bean = new FilterBean();
- 			bean.setColumnName("Status");
- 			bean.setColumnValues(Status);
- 			bean.setAnd_or("And");
- 			bean.setSearchCriteria("in");
- 			
+ 			 bean.setColumnName("Status");
+ 			 bean.setColumnValues(Status);
+ 			 bean.setAnd_or("And");
+ 			 bean.setSearchCriteria("in"); 			
  		}
     	 return bean;
      }
@@ -256,162 +246,169 @@ public abstract class SearchCriteriaType extends JPanel
     	 FilterBean bean = null;
     	 if(!commonUTIL.isEmpty(Action)) {
     		 bean = new FilterBean();
- 			bean.setColumnName("Action");
- 			bean.setColumnValues(Action);
- 			bean.setSearchCriteria("in");
- 			bean.setAnd_or("And");
- 			
+ 			 bean.setColumnName("Action");
+ 			 bean.setColumnValues(Action);
+ 			 bean.setSearchCriteria("in");
+ 			 bean.setAnd_or("And");
  		}
     	 return bean;
      }
+     
      public FilterBean getCurrency(String Currency, String colName)  {
     	 FilterBean bean = null;
     	 if(!commonUTIL.isEmpty(Currency)) {
     		 bean = new FilterBean();
-    			bean.setColumnName(colName);
-    			
-    			bean.setColumnValues(Currency);
-    			bean.setAnd_or("And");
-    			bean.setSearchCriteria("in");
- 			
+    		 bean.setColumnName(colName);
+    		 bean.setColumnValues(Currency);
+    		 bean.setAnd_or("And");
+    		 bean.setSearchCriteria("in"); 			
  		}
     	 return bean;
      }
+     
      public FilterBean getCurrency(MultiSelectListExComboBox values, String colName)  {
     	 FilterBean  bean = new FilterBean();
     	 String ss = "";
     	 String idSelected ="";
     	 int ids [] = values.getSelectedIndices();
-			Object obj[] = values.getSelectedObjects();
-			for (int i = 0; i < values.getSelectedObjects().length; i++) {
-				    ss = ss + (String) obj[i] + ",";
-				    idSelected = idSelected + ids[i] +",";
-			}
-			bean.setColumnName("Currency");
-			bean.setColumnValues(ss.substring(0, ss.length()-1));
-			bean.setIdSelected(idSelected.substring(0, idSelected.length()-1));
-			bean.setSearchCriteria("in");
- 			bean.setAnd_or("And");
+		 Object obj[] = values.getSelectedObjects();
+		 for (int i = 0; i < values.getSelectedObjects().length; i++) {
+			    ss = ss + (String) obj[i] + ",";
+			    idSelected = idSelected + ids[i] +",";
+		 }
+		
+		 bean.setColumnName(colName);
+		 bean.setColumnValues(ss.substring(0, ss.length()-1));
+		 bean.setIdSelected(idSelected.substring(0, idSelected.length()-1));
+		 bean.setSearchCriteria("in");
+		 bean.setAnd_or("And");
+ 			
     	 return bean;
      }
+     
      public FilterBean getProductSubType(MultiSelectListExComboBox values, String colName)  {
     	 FilterBean  bean = new FilterBean();
     	 String ss = "";
     	 String idSelected ="";
     	 int ids [] = values.getSelectedIndices();
-			Object obj[] = values.getSelectedObjects();
-			for (int i = 0; i < values.getSelectedObjects().length; i++) {
-				    ss = ss + (String) obj[i] + ",";
-				    idSelected = idSelected + ids[i] +",";
-			}
-			bean.setColumnName(colName);
-			bean.setColumnValues(ss.substring(0, ss.length()-1));
-			bean.setIdSelected(idSelected.substring(0, idSelected.length()-1));
-			bean.setSearchCriteria("in");
- 			bean.setAnd_or("And");
-    	 return bean;
+		 Object obj[] = values.getSelectedObjects();
+		 for (int i = 0; i < values.getSelectedObjects().length; i++) {
+			    ss = ss + (String) obj[i] + ",";
+			    idSelected = idSelected + ids[i] +",";
+		 }
+
+		 bean.setColumnName(colName);
+		 bean.setColumnValues(ss.substring(0, ss.length()-1));
+		 bean.setIdSelected(idSelected.substring(0, idSelected.length()-1));
+		 bean.setSearchCriteria("in");
+		 bean.setAnd_or("And");
+    	
+		 return bean;
      }
+     
      public FilterBean getprimaryCurrency(String values, String colName)  {
     	 FilterBean  bean = new FilterBean();
     	 String ss = "";
     	 String idSelected ="";
     	
-			bean.setColumnName("PrimaryCurrLedger");
-			bean.setColumnValues(values);
+    	 bean.setColumnName("PrimaryCurrLedger");
+		 bean.setColumnValues(values);
 			//bean.setIdSelected(idSelected.substring(0, idSelected.length()-1));
-			bean.setSearchCriteria("in");
- 			bean.setAnd_or("or");
-    	 return bean;
+		 bean.setSearchCriteria("in");
+ 		 bean.setAnd_or("or");
+    	 
+ 		 return bean;
      }
+     
      public FilterBean getquotingCurrency(String values, String colName)  {
     	 FilterBean  bean = new FilterBean();
     	 String ss = "";
     	 String idSelected ="";
     	
-			bean.setColumnName("QuotingCurrLedger");
-			bean.setColumnValues(values);
-			//bean.setIdSelected(idSelected.substring(0, idSelected.length()-1));
-			bean.setSearchCriteria("in");
- 			bean.setAnd_or("And");
-    	 return bean;
+		 bean.setColumnName("QuotingCurrLedger");
+		 bean.setColumnValues(values);
+		 //bean.setIdSelected(idSelected.substring(0, idSelected.length()-1));
+		 bean.setSearchCriteria("in");
+ 		 bean.setAnd_or("And");
+    	 
+ 		 return bean;
      }
      public FilterBean getProductType(String ProductType)  {
     	 FilterBean bean = null;
     	 if(!commonUTIL.isEmpty(ProductType)) {
-    		 bean = new FilterBean();
+    		bean = new FilterBean();
  			bean.setColumnName("ProductType");
  			bean.setColumnValues(ProductType);
  			bean.setAnd_or("And");
  			bean.setSearchCriteria("in");
  			
  		}
-    	 return bean;
+    	 
+    	return bean;
      }
      public FilterBean getProductSubType(String ProductSubType)  {
     	 FilterBean bean = null;
     	 if(!commonUTIL.isEmpty(ProductSubType)) {
-    		 bean = new FilterBean();
+    		bean = new FilterBean();
  			bean.setColumnName("ProductSubType");
  			bean.setColumnValues(ProductSubType);
  			bean.setSearchCriteria("in");
- 			bean.setAnd_or("And");
- 			
+ 			bean.setAnd_or("And"); 			
  		}
-    	 return bean;
+    	 
+    	return bean;
      }
      
      public FilterBean getTransferType(String transferType)  {
     	 FilterBean bean = null;
     	 if(!commonUTIL.isEmpty(transferType)) {
-    		 bean = new FilterBean();
+    		bean = new FilterBean();
  			bean.setColumnName("TransferType");
  			bean.setColumnValues(transferType);
  			bean.setSearchCriteria("in");
  			bean.setAnd_or("And");
  			
  		}
-    	 return bean;
+    	 
+    	return bean;
      }
     
      public FilterBean getTransferEventType(String transferEventType)  {
     	 FilterBean bean = null;
     	 if(!commonUTIL.isEmpty(transferEventType)) {
-    		 bean = new FilterBean();
+    		bean = new FilterBean();
  			bean.setColumnName("TransferEventType");
  			bean.setColumnValues(transferEventType);
  			bean.setSearchCriteria("in");
- 			bean.setAnd_or("And");
- 			
+ 			bean.setAnd_or("And"); 			
  		}
-    	 return bean;
+    	
+    	return bean;
      }
      
      public FilterBean getTransferMethodType(String transferMethodType)  {
     	 FilterBean bean = null;
     	 if(!commonUTIL.isEmpty(transferMethodType)) {
-    		 bean = new FilterBean();
+    		bean = new FilterBean();
  			bean.setColumnName("TransferMethodType");
  			bean.setColumnValues(transferMethodType);
  			bean.setSearchCriteria("in");
- 			bean.setAnd_or("And");
- 			
+ 			bean.setAnd_or("And"); 			
  		}
-    	 return bean;
+    	 
+    	return bean;
      }
      
      public FilterBean getBookName(int bookID)  {
          FilterBean bean = null;
          if(bookID > 0) {
-                 bean = new FilterBean();
-                        bean.setColumnName("Book");
-                        bean.setSearchCriteria("in");
-                        
-                        bean.setIdSelected(new Integer(getBookID(bookID)).toString());
-                        bean.setAnd_or("And");
-                
-                        
-                        
-                }
+        	 bean = new FilterBean();
+             bean.setColumnName("Book");
+             bean.setSearchCriteria("in");
+             bean.setIdSelected(new Integer(getBookID(bookID)).toString());
+             bean.setAnd_or("And");
+         }
+         
          return bean;
      }
      
@@ -424,11 +421,9 @@ public abstract class SearchCriteriaType extends JPanel
 		
     	 Object obj[] = values.getSelectedObjects();
 		
-    	 for (int i = 0; i < values.getSelectedObjects().length; i++) {
-				    
+    	 for (int i = 0; i < values.getSelectedObjects().length; i++) {				    
     		 ss = ss + (String) obj[i] + ",";
-			 idSelected = idSelected + ids[i] +",";
-			 
+			 idSelected = idSelected + ids[i] +",";			 
     	 }
     	
     	 bean = new FilterBean();
@@ -440,33 +435,27 @@ public abstract class SearchCriteriaType extends JPanel
  		 			
     	 return bean;
      } 
+     
      public FilterBean getLegalEntity(int leId, String role)  {
     	 FilterBean bean = null;
     	
-    	 if(leId > -1) {
-    		 
+    	 if(leId > -1) {    		 
     		 bean = new FilterBean();
  			 bean.setColumnName(role);
  			 
- 			 if (role.equals("cpid")) {
- 				 
- 				bean.setColumnValues(new Integer(getCPid(leId)).toString());
- 				
- 			 } else if (role.equals("poId")) {
- 				 
- 				bean.setColumnValues(new Integer(getPOid(leId)).toString());
- 			 
- 			 } else if (role.equals("agentId")) {
- 				 
-  				bean.setColumnValues(new Integer(getAgentid(leId)).toString());
-  			 
+ 			 if (role.equals("cpid")) { 				 
+ 				bean.setColumnValues(new Integer(getCPid(leId)).toString()); 				
+ 			 } else if (role.equals("poId")) { 				 
+ 				bean.setColumnValues(new Integer(getPOid(leId)).toString()); 			 
+ 			 } else if (role.equals("agentId")) { 				 
+  				bean.setColumnValues(new Integer(getAgentid(leId)).toString());  			 
  			 }
  			 
  			 bean.setAnd_or("And");
- 			 bean.setSearchCriteria("in");
- 			
+ 			 bean.setSearchCriteria("in"); 			
  		}
-    	 return bean;
+    	 
+    	return bean;
      }
      
      public int getBookID(int idSelected) {
@@ -549,68 +538,43 @@ public abstract class SearchCriteriaType extends JPanel
     	*/
     	return splitBookNames;
     }
+    
      public void processLEDataCombo1(javax.swing.DefaultComboBoxModel combodata, Hashtable ids, String role) {
- 		
- 		Vector ledata;
- 		
- 				//String roleType = " role like 'PO' ";
- 			ledata = (Vector<String>) getFilterValues().getLegalEntitys();
+    	 Vector ledata;
+ 		 ledata = (Vector<String>) getFilterValues().getLegalEntitys();
 
- 			Iterator it = ledata.iterator();
- 			int p = 0;
- 			combodata.insertElementAt("", p++);
+ 		 Iterator it = ledata.iterator();
+ 		 int p = 0;
+ 		 combodata.insertElementAt("", p++);
  			
- 			while (it.hasNext()) {
+ 		 while (it.hasNext()) {
+ 			 LegalEntity le = (LegalEntity) it.next();
+ 			 if (le.getRole().equalsIgnoreCase(role)) {
+ 				 combodata.insertElementAt(le.getName(), p++);
+ 	 			 ids.put(p, le);
+ 	 		 }
+ 		}
 
- 				LegalEntity le = (LegalEntity) it.next();
- 				
- 				if (le.getRole().equalsIgnoreCase(role)) {
- 					
- 					combodata.insertElementAt(le.getName(), p++);
- 	 				ids.put(p, le);
- 	 				
- 	 				
- 				}
- 			
- 			}
+ 	} 	
 
- 		
+     protected void processBookDataCombo1(javax.swing.DefaultComboBoxModel combodata, Hashtable ids) {    		
+    	Vector ledata;    	
+    	ledata = (Vector) getFilterValues().getBooks();
 
- 	}
- 	
-
-     protected void processBookDataCombo1(javax.swing.DefaultComboBoxModel combodata, Hashtable ids) {
-    		
-    		Vector ledata;
-    		
-    				//String roleType = " role like 'PO' ";
-    			ledata = (Vector) getFilterValues().getBooks();
-
-    			Iterator it = ledata.iterator();
-    			int p = 1;
-    			combodata.addElement("");
-    			while (it.hasNext()) {
-
-    				Book book = (Book) it.next();
-
-    				combodata.insertElementAt(book.getBook_name(), p);
-    				ids.put(p, book);
-    				
-    				p++;
-    			
-    			}
-
-    		
-
-    	}
-     
+		Iterator it = ledata.iterator();
+		int p = 1;
+		combodata.addElement("");
+		while (it.hasNext()) {
+			Book book = (Book) it.next();
+			combodata.insertElementAt(book.getBook_name(), p);
+			ids.put(p, book);		
+			p++;
+		}
+   	}     
 
  	public void processLEDataCombo1(DefaultComboBoxModel legalEntityData2,
- 			Hashtable<Integer, LegalEntity> counterPartyID2) {
- 		// TODO Auto-generated method stub
+ 			Hashtable<Integer, LegalEntity> counterPartyID2) { 		
  		Vector ledata;
- 		
-			//String roleType = " role like 'PO' ";
 		ledata = (Vector<String>) getFilterValues().getLegalEntitys();
 
 		Iterator it = ledata.iterator();
@@ -618,42 +582,43 @@ public abstract class SearchCriteriaType extends JPanel
 		legalEntityData2.addElement("");
 		
 		while (it.hasNext()) {
-
 			LegalEntity le = (LegalEntity) it.next();
-			
-		
-				
-				legalEntityData2.insertElementAt(le.getName(), p);
-				counterPartyID2.put(p, le);
-				p++;
-				
-		
-		
+			legalEntityData2.insertElementAt(le.getName(), p);
+			counterPartyID2.put(p, le);
+			p++;		
 		}
  	}
-     
-     protected void processDomainData(javax.swing.DefaultComboBoxModel combodata, Vector<String> domainData) {
-    		
-    		Vector ledata;
-    		
-    				//String roleType = " role like 'PO' ";
-    			ledata =domainData;
+ 	
+ 	protected void processDomainData(javax.swing.DefaultComboBoxModel combodata, Vector<String> domainData) { 		
+ 		Vector ledata;    		
+		ledata =domainData;
 
-    			Iterator it = ledata.iterator();
-    			int p = 0;
-    			combodata.insertElementAt("", p++);
-    			while (it.hasNext()) {
-
-    				StartUPData stData = (StartUPData) it.next();
-
-    				combodata.insertElementAt(stData.getName(), p++);
-    				
-    			
-    			}
-
-    		
-
-    	}
-     
-     
+		Iterator it = ledata.iterator();
+		int p = 0;
+		combodata.insertElementAt("", p++);
+		while (it.hasNext()) {
+			StartUPData stData = (StartUPData) it.next();
+			combodata.insertElementAt(stData.getName(), p++);	
+		}
+		
+ 	}
+      	  
+ 	protected void processCurrenyPairData(javax.swing.DefaultComboBoxModel combodata) { 		
+ 		
+ 		Vector<CurrencyPair> vector = null;
+		try {
+			vector = (Vector<CurrencyPair>) remoteBORef.selectALLCurrencyPair();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+ 		Iterator it = vector.iterator();
+ 		
+ 		int p = 0;
+		combodata.insertElementAt("", p++);
+		while (it.hasNext()) {
+			CurrencyPair data = (CurrencyPair) it.next();
+			combodata.insertElementAt(data.getPrimary_currency()+"/"+data.getQuoting_currency(), p++);	
+		}		
+ 	}
+ 
 }
