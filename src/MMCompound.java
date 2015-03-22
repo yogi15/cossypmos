4794b3815ae3a0a9f35cbf0d60ae52cb26e356c5
yogi.kadam@gmail.com <yogi.kadam@gmail.com@270f5cef-2aaa-b512-6b27-43a3548ac535>
2015-03-22 07:58:07 +0000

    MMWindows added in default package

---
+++ src/MMCompound.java
@@ -0,0 +1,91 @@
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.dyno.visual.swing.layouts.Constraints;
+import org.dyno.visual.swing.layouts.GroupLayout;
+import org.dyno.visual.swing.layouts.Leading;
+
+
+//VS4E -- DO NOT REMOVE THIS LINE!
+public class MMCompound extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	private JLabel jLabel0;
+	private JLabel jLabel1;
+	private JLabel jLabel2;
+	private JComboBox jComboBox2;
+	private JComboBox jComboBox1;
+	private JComboBox jComboBox0;
+
+	public MMCompound() {
+		initComponents();
+	}
+
+	private void initComponents() {
+		setLayout(new GroupLayout());
+		add(getJLabel0(), new Constraints(new Leading(21, 10, 10), new Leading(12, 12, 12)));
+		add(getJLabel1(), new Constraints(new Leading(191, 10, 10), new Leading(12, 12, 12)));
+		add(getJLabel2(), new Constraints(new Leading(377, 10, 10), new Leading(12, 12, 12)));
+		add(getJComboBox2(), new Constraints(new Leading(447, 10, 10), new Leading(8, 12, 12)));
+		add(getJComboBox1(), new Constraints(new Leading(268, 12, 12), new Leading(8, 12, 12)));
+		add(getJComboBox0(), new Constraints(new Leading(76, 12, 12), new Leading(8, 12, 12)));
+		setSize(617, 50);
+	}
+
+	private JComboBox getJComboBox0() {
+		if (jComboBox0 == null) {
+			jComboBox0 = new JComboBox();
+			jComboBox0.setModel(new DefaultComboBoxModel(new Object[] { "item0", "item1", "item2", "item3" }));
+			jComboBox0.setDoubleBuffered(false);
+			jComboBox0.setBorder(null);
+		}
+		return jComboBox0;
+	}
+
+	private JComboBox getJComboBox1() {
+		if (jComboBox1 == null) {
+			jComboBox1 = new JComboBox();
+			jComboBox1.setModel(new DefaultComboBoxModel(new Object[] { "item0", "item1", "item2", "item3" }));
+			jComboBox1.setDoubleBuffered(false);
+			jComboBox1.setBorder(null);
+		}
+		return jComboBox1;
+	}
+
+	private JComboBox getJComboBox2() {
+		if (jComboBox2 == null) {
+			jComboBox2 = new JComboBox();
+			jComboBox2.setModel(new DefaultComboBoxModel(new Object[] { "item0", "item1", "item2", "item3" }));
+			jComboBox2.setDoubleBuffered(false);
+			jComboBox2.setBorder(null);
+		}
+		return jComboBox2;
+	}
+
+	private JLabel getJLabel2() {
+		if (jLabel2 == null) {
+			jLabel2 = new JLabel();
+			jLabel2.setText("Cmp Freq");
+		}
+		return jLabel2;
+	}
+
+	private JLabel getJLabel1() {
+		if (jLabel1 == null) {
+			jLabel1 = new JLabel();
+			jLabel1.setText("Cmp Method");
+		}
+		return jLabel1;
+	}
+
+	private JLabel getJLabel0() {
+		if (jLabel0 == null) {
+			jLabel0 = new JLabel();
+			jLabel0.setText("Pay Freq");
+		}
+		return jLabel0;
+	}
+
+}
