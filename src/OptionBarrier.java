a9b8e35a0a8ed8c73760de5c5d4eed1e2a0d19fd
yogi.kadam@gmail.com <yogi.kadam@gmail.com@270f5cef-2aaa-b512-6b27-43a3548ac535>
2015-05-31 08:28:39 +0000

    OptionBarrier.java: first file in default package

---
+++ src/OptionBarrier.java
@@ -0,0 +1,230 @@
+
+public class OptionBarrier {
+	
+	private  String barriertype;
+	private  String barrierDirection;
+	private  double barrierLevel;
+	private  String monitoring;
+	private  double rebate;
+	private  String startDate;
+	private  String emdDate;
+	private  String rebatePayDate;
+	private  double rebateIsDateLag;
+	private  String rebateDateRoll;
+	private  double rebateLagOffset;
+	private  double rebateBusDays;
+	private  String rebateHolidays;
+	private  String barrierStatus;
+	private  String rebateCondition;
+	private  String rebateCurrency;
+	private  String rebateWay;
+	private  String rebateTimingType;
+	private  int underlyingId;
+	private  int isFwdStarting;
+	private  int isPerfBased;
+	private  String priority;
+	private  int orderId;
+	private  int productId;
+	private  int startTime;
+	private  String startTimezone;
+	private  double endTime;
+	private  String endTimezone;
+	private  String duration;
+	private  String observationSource;
+	private  String stageStartDate;
+	private  String stageEndDate;
+	
+	public String getBarriertype() {
+		return barriertype;
+	}
+	public void setBarriertype(String barriertype) {
+		this.barriertype = barriertype;
+	}
+	public String getBarrierDirection() {
+		return barrierDirection;
+	}
+	public void setBarrierDirection(String barrierDirection) {
+		this.barrierDirection = barrierDirection;
+	}
+	public double getBarrierLevel() {
+		return barrierLevel;
+	}
+	public void setBarrierLevel(double barrierLevel) {
+		this.barrierLevel = barrierLevel;
+	}
+	public String getMonitoring() {
+		return monitoring;
+	}
+	public void setMonitoring(String monitoring) {
+		this.monitoring = monitoring;
+	}
+	public double getRebate() {
+		return rebate;
+	}
+	public void setRebate(double rebate) {
+		this.rebate = rebate;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEmdDate() {
+		return emdDate;
+	}
+	public void setEmdDate(String emdDate) {
+		this.emdDate = emdDate;
+	}
+	public String getRebatePayDate() {
+		return rebatePayDate;
+	}
+	public void setRebatePayDate(String rebatePayDate) {
+		this.rebatePayDate = rebatePayDate;
+	}
+	public double getRebateIsDateLag() {
+		return rebateIsDateLag;
+	}
+	public void setRebateIsDateLag(double rebateIsDateLag) {
+		this.rebateIsDateLag = rebateIsDateLag;
+	}
+	public String getRebateDateRoll() {
+		return rebateDateRoll;
+	}
+	public void setRebateDateRoll(String rebateDateRoll) {
+		this.rebateDateRoll = rebateDateRoll;
+	}
+	public double getRebateLagOffset() {
+		return rebateLagOffset;
+	}
+	public void setRebateLagOffset(double rebateLagOffset) {
+		this.rebateLagOffset = rebateLagOffset;
+	}
+	public double getRebateBusDays() {
+		return rebateBusDays;
+	}
+	public void setRebateBusDays(double rebateBusDays) {
+		this.rebateBusDays = rebateBusDays;
+	}
+	public String getRebateHolidays() {
+		return rebateHolidays;
+	}
+	public void setRebateHolidays(String rebateHolidays) {
+		this.rebateHolidays = rebateHolidays;
+	}
+	public String getBarrierStatus() {
+		return barrierStatus;
+	}
+	public void setBarrierStatus(String barrierStatus) {
+		this.barrierStatus = barrierStatus;
+	}
+	public String getRebateCondition() {
+		return rebateCondition;
+	}
+	public void setRebateCondition(String rebateCondition) {
+		this.rebateCondition = rebateCondition;
+	}
+	public String getRebateCurrency() {
+		return rebateCurrency;
+	}
+	public void setRebateCurrency(String rebateCurrency) {
+		this.rebateCurrency = rebateCurrency;
+	}
+	public String getRebateWay() {
+		return rebateWay;
+	}
+	public void setRebateWay(String rebateWay) {
+		this.rebateWay = rebateWay;
+	}
+	public String getRebateTimingType() {
+		return rebateTimingType;
+	}
+	public void setRebateTimingType(String rebateTimingType) {
+		this.rebateTimingType = rebateTimingType;
+	}
+	public int getUnderlyingId() {
+		return underlyingId;
+	}
+	public void setUnderlyingId(int underlyingId) {
+		this.underlyingId = underlyingId;
+	}
+	public int getIsFwdStarting() {
+		return isFwdStarting;
+	}
+	public void setIsFwdStarting(int isFwdStarting) {
+		this.isFwdStarting = isFwdStarting;
+	}
+	public int getIsPerfBased() {
+		return isPerfBased;
+	}
+	public void setIsPerfBased(int isPerfBased) {
+		this.isPerfBased = isPerfBased;
+	}
+	public String getPriority() {
+		return priority;
+	}
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public int getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(int startTime) {
+		this.startTime = startTime;
+	}
+	public String getStartTimezone() {
+		return startTimezone;
+	}
+	public void setStartTimezone(String startTimezone) {
+		this.startTimezone = startTimezone;
+	}
+	public double getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(double endTime) {
+		this.endTime = endTime;
+	}
+	public String getEndTimezone() {
+		return endTimezone;
+	}
+	public void setEndTimezone(String endTimezone) {
+		this.endTimezone = endTimezone;
+	}
+	public String getDuration() {
+		return duration;
+	}
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+	public String getObservationSource() {
+		return observationSource;
+	}
+	public void setObservationSource(String observationSource) {
+		this.observationSource = observationSource;
+	}
+	public String getStageStartDate() {
+		return stageStartDate;
+	}
+	public void setStageStartDate(String stageStartDate) {
+		this.stageStartDate = stageStartDate;
+	}
+	public String getStageEndDate() {
+		return stageEndDate;
+	}
+	public void setStageEndDate(String stageEndDate) {
+		this.stageEndDate = stageEndDate;
+	}
+		
+}
