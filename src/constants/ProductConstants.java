148129f9b176047088c8675afa2edcdc53729460
pankajmahadik24@gmail.com <pankajmahadik24@gmail.com@270f5cef-2aaa-b512-6b27-43a3548ac535>
2015-06-04 15:59:39 +0000

    ProductConstants.java,TradeConstants

--- src/constants/ProductConstants.java
+++ src/constants/ProductConstants.java
@@ -6,5 +6,17 @@ public class ProductConstants {
 	final public static String PRINCIPAL = "Principal";
 	final public static String COMPOUNDEDINTEREST = "CompoundedInterest";
 	final public static String FUTURECONTRACT = "FUTURECONTRACT";
+	final public static String FXOPTION = "FXOption";
+	final public static String FX  = "FX";
+	final public static String RATETYPEFIXED = "Fixed";
+	final public static String RATETYPEFLOAT = "Float";
+	public static final String PRODUCTTYPEBOND = "BOND";
+
+	public static final String PRODUCTTYPEFX = "FX";
+
+	public static final String PRODUCTTYPEFXOPTION = "FXOPION";
+
+	public static final String PRODUCTTYPEMM = "MM";
+	
 	
 }
--- src/constants/TradeConstants.java
+++ src/constants/TradeConstants.java
@@ -10,5 +10,9 @@ public class TradeConstants {
 	public static final String TRADER = "Trader";
 	public static final String BOOK = "Book";
 	
+	public static final String BLANK = "";
+	public static final String TRADEACTIONNEW  = "NEW";
+	public static final String TRADESTATUSNONE  = "NONE";
+	public static final String ZERO = "0";
 	public static final String TRADE_TYPE_FUTURECONTRACT = "FUTURECONTRACT";
 }
