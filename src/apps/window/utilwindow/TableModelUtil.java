fca699a044a8357a53972cad2f74b378fd810f61
yogi.kadam@gmail.com <yogi.kadam@gmail.com@270f5cef-2aaa-b512-6b27-43a3548ac535>
2015-06-14 16:02:46 +0000

    TableModelUtil.java: first file

--- src/apps/window/utilwindow/TableModelUtil.java
+++ src/apps/window/utilwindow/TableModelUtil.java
@@ -4,88 +4,95 @@ import java.util.Vector;
 
 import javax.swing.table.AbstractTableModel;
 
+import beans.Attribute;
 import beans.Folder;
 
-public class TableModelUtil extends AbstractTableModel {   
-    
- final String[] columnNames;  
-    
- final Vector<Folder> data;   
-        
-        
- public TableModelUtil( Vector<Folder> myData,String col [] ) {   
- 	this.columnNames = col;
-this.data = myData;   
-}   
+class TableModelUtil extends AbstractTableModel {
+    final String[] columnNames;
 
-    
- public int getColumnCount() {   
-     return columnNames.length;   
- }   
-    
- public int getRowCount() {   
-     return data.size();   
- }   
- public String getColumnName(int col) {   
-     return columnNames[col];   
- }   
- public Object getValueAt(int row, int col) {   
-     Object value = null;  	 
- 
-	 Folder folder = (Folder) data.get(row);
-	 switch (col) {
-     case 0:
-         value = folder.getId();
-         break;
-     case 1:
-         value = folder.getFolder_name();
-         break;
-	 }
-     return value;
- }   
-   
- public boolean isCellEditable(int row, int col) {   
- return false;   
- }   
- public void setValueAt(Object value, int row, int col) {   
-         System.out.println("Setting value at " + row + "," + col   
-                            + " to " + value   
-                            + " (an instance of "    
-                            + value.getClass() + ")");  
-         if(value instanceof Folder) {
-     data.set(row,(Folder) value) ;
-     this.fireTableDataChanged();   
-         System.out.println("New value of data:");   
-         }
+    Vector<Attribute> data;
+
+    public TableModelUtil(Vector<Attribute> myData, String col[]) {
+            this.columnNames = col;
+            this.data = myData;
+    }
+
+    public int getColumnCount() {
+            return columnNames.length;
+    }
+
+    public int getRowCount() {
+            if (data != null)
+                    return data.size();
+            return 0;
+    }
+
+    public String getColumnName(int col) {
+        return columnNames[col];
+    }
+
+    public Object getValueAt(int row, int col) {
+        Object value = null;
+
+        Attribute attribute = (Attribute) data.get(row);
+
+        switch (col) {
+
+        case 0:
+                value = attribute.getName();
+                break;
+        case 1:
+                value = attribute.getValue()==null?"":attribute.getValue();
+                break;
         
- }   
+        }
+        return value;
+    }
+
+    public boolean isCellEditable(int row, int col) {   
+    	  if (col == 0)       //first column will be uneditable   
+              return false;
+        
+        return true;            
+    }
     
- public void addRow(Object value) {   
-    
-	 data.add((Folder) value) ;
- this.fireTableDataChanged();   
-   
- }   
-    
- public void delRow(int row) {   
-    
- data.remove(row);          
- this.fireTableDataChanged();   
-    
- }   
- 
- public void udpateValueAt(Object value, int row, int col) {   
-     System.out.println("Setting value at " + row + "," + col   
-                        + " to " + value   
-                        + " (an instance of "    
-                        + value.getClass() + ")");   
-  
-     data.set(row,(Folder) value) ;
- fireTableCellUpdated(row, col);   
-     System.out.println("New value of data:");   
-    
-}   
-    
-    
-    
-}   
+    public void setValueAt(Object value, int row, int col) {
+    	if(row == -1)
+    		return;
+        if (value == null)
+                return;
+    //    System.out.println("Setting value at " + row + "," + col + " to "
+    //                    + value + " (an instance of " + value.getClass() + ")");
+        Attribute ff = (Attribute)      data.get(row);
+        ff.setValue((String) value);
+
+        data.set(row, ff);
+        this.fireTableDataChanged();
+            //System.out.println("New value of data:");           
+    }
+
+    public void addRow(Object value) {
+        data.add((Attribute) value);
+        this.fireTableDataChanged();
+    }
+
+    public void delRow(int row) {
+        if (row != -1) {
+            data.remove(row);
+            this.fireTableDataChanged();
+        }
+    }
+
+    public void udpateValueAt(Object value, int row, int col) {
+        data.set(row, (Attribute) value);
+        for (int i = 0; i < columnNames.length; i++)
+            fireTableCellUpdated(row, i);
+    }
+    public void removeALL() {
+        if (data != null) {
+            data.removeAllElements();
+        }
+        // data = null;
+        this.fireTableDataChanged();
+    }
+}
