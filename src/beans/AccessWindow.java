aa0233de0969c35ff16b811edcb635dd13f29cfa
yogi.kadam <yogi.kadam@gmail.com>
2015-06-16 03:36:42 +0000

    AccessWindow.java: first file

---
+++ src/beans/AccessWindow.java
@@ -0,0 +1,29 @@
+package beans;
+
+public class AccessWindow {
+
+	private String groupname;
+	private String windowName;
+	int isAccessable;
+	
+	public String getGroupname() {
+		return groupname;
+	}
+	public void setGroupname(String groupname) {
+		this.groupname = groupname;
+	}
+	public String getWindowName() {
+		return windowName;
+	}
+	public void setWindowName(String windowName) {
+		this.windowName = windowName;
+	}
+	public int getIsAccessable() {
+		return isAccessable;
+	}
+	public void setIsAccessable(int isAccessable) {
+		this.isAccessable = isAccessable;
+	}
+	
+	
+}
