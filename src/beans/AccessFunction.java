e97cd1f40f1813dc60a241f9dd81eb993c1a83fc
yogi.kadam <yogi.kadam@gmail.com>
2015-06-16 03:37:25 +0000

    AccessFunction.java: first file

---
+++ src/beans/AccessFunction.java
@@ -0,0 +1,36 @@
+package beans;
+
+public class AccessFunction {
+	
+	private String groupname;
+	private String windowName;
+	private String functionName;
+	int isAccessable;
+	
+	public String getGroupname() {
+		return groupname;
+	}
+	public void setGroupname(String groupname) {
+		this.groupname = groupname;
+	}
+	public String getWindowName() {
+		return windowName;
+	}
+	public void setWindowName(String windowName) {
+		this.windowName = windowName;
+	}
+	public String getFunctionName() {
+		return functionName;
+	}
+	public void setFunctionName(String functionName) {
+		this.functionName = functionName;
+	}
+	public int getIsAccessable() {
+		return isAccessable;
+	}
+	public void setIsAccessable(int isAccessable) {
+		this.isAccessable = isAccessable;
+	}
+	
+	
+}
