2a114e8ee4ab3396e184df5b2f0b413c921c2487
pankajmahadik24@gmail.com <pankajmahadik24@gmail.com@270f5cef-2aaa-b512-6b27-43a3548ac535>
2015-06-20 08:30:16 +0000

    RemoteProduct.java ,RemoteTrade.java

--- src/dsServices/RemoteTrade.java
+++ src/dsServices/RemoteTrade.java
@@ -6,6 +6,7 @@ import java.util.Vector;
 import java.rmi.Remote;
 
 import dsEventProcessor.EventProcessor;
+
 import beans.EventController;
 import beans.Trade;
 import beans.WFConfig;
@@ -52,6 +53,6 @@ public interface RemoteTrade extends Remote {
 //	public Trade undo(Trade trade, int tradeVersion) throws RemoteException;
 	
 	public String selectTradeAttributesAsString(String tradeId) throws RemoteException;
-	public Collection getFTDReport(String sql) throws RemoteException;
+	public   Collection getFTDReport(String sql) throws RemoteException;
 	public Collection getVarReport(String sql) throws RemoteException;
 }
